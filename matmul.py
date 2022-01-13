233b5834ad5d0215890349f6ffee9d4a04e73dd1
Tommie_Cheng <tommie_cheng@bb.com>
2022-01-13 16:36:24 +0800

    20220113

--- matmul.py
+++ matmul.py
@@ -25,7 +25,7 @@ class SGEMMShader:
 	def __init__(self,MKN,gpuIdx=0,logLevel=logging.NOTSET):
 		self.kp_logger = logging.getLogger("kp")
 		self.kp_logger.setLevel(logLevel)
-		naive_matmul_shader_code = """
+		naive_matmul_shader_code = '''
 				#version 450
 				layout (local_size_x = 1, local_size_y = 1) in;
 				layout (set = 0, binding = 0) readonly buffer buf_in_tensor_1 { float A[]; };
@@ -45,13 +45,13 @@ class SGEMMShader:
 						AB_mn += A[k*M+m]*B[n*K+k]; // parallel with (M,N,1)
 					C[(m*N)+n] = AB_mn;
 				}//end_main
-		"""
+		'''
 
-		
+
 		TS = 32
 		WPT = 8
 		RTS = 4
-		opt_shader_code = '''
+		tiled_shader_code = '''
 		#version 450
 		layout (local_size_x = {TS}, local_size_y = {RTS}) in;
 		layout (set = 0, binding = 0) readonly buffer buf_in_tensor_1 {{ float in_tensor_1[]; }};
@@ -101,8 +101,110 @@ class SGEMMShader:
 		    	out_tensor[M*(globalCol+w*{RTS}) + globalRow] = acc[w];
 		    }}//endfor
 		}}'''
+
+		twod_register_code = '''
+		#version 450
+		#define TSM 128                     // The tile-size in dimension M
+		#define TSN 128                     // The tile-size in dimension N
+		#define TSK 16                      // The tile-size in dimension K
+		#define WPTM 8                      // The amount of work-per-thread in dimension M
+		#define WPTN 8                      // The amount of work-per-thread in dimension N
+		#define LPTA ((TSK*WPTM*WPTN)/(TSN)) // The amount of loads-per-thread for A
+		#define LPTB ((TSK*WPTM*WPTN)/(TSM)) // The amount of loads-per-thread for B
+		#define RTSM 16    // The reduced tile-size in dimension M (TSM/WPTM number of threads)
+		#define RTSN 16    // The reduced tile-size in dimension N (TSN/WPTN number of threads)
+		#define MOD2(x,y) ((x) % (y))
+		#define DIV2(x,y) ((x) / (y))
+
+		layout (local_size_x = RTSM, local_size_y = RTSN, local_size_z = 1) in;
+		layout (set=0, binding = 0) readonly buffer ssbA {float A[];};
+		layout (set=0, binding = 1) readonly buffer ssbB {float B[];};
+		layout (set=0, binding = 2) writeonly buffer ssbC {float C[];};
+		layout (constant_id = 0) const float Mf = 0;
+		layout (constant_id = 1) const float Kf = 0;
+		layout (constant_id = 2) const float Nf = 0;
+		shared float Asub[TSK][TSM];    // Local memory to fit a tile of A and B
+		shared float Bsub[TSN][TSK+2];
+		
+
+
+		void main() {
+			int M = int(Mf);
+		    int K = int(Kf);
+		    int N = int(Nf);
+
+		    // Thread identifiers
+		    int tidm = int(gl_LocalInvocationID.x);  // Local row ID (max: TSM/WPTM == RTSM)
+		    int tidn = int(gl_LocalInvocationID.y);  // Local col ID (max: TSN/WPTN == RTSN)
+		    int offsetM = TSM*int(gl_WorkGroupID.x); // Work-group offset
+		    int offsetN = TSN*int(gl_WorkGroupID.y); // Work-group offset
+
+		    // Allocate register space
+		    float Areg;
+		    float Breg[WPTN];
+		    float acc[WPTM][WPTN];
+
+		    // Initialise the accumulation registers
+		    for (int wm=0; wm < WPTM; wm++) {
+		        for (int wn=0; wn < WPTN; wn++) {
+		            acc[wm][wn] = 0.0;
+		        }
+		    }
+		    // Loop over all tiles
+		    int numTiles = K/TSK;
+		    int t=0;
+		    do {
+		        // Load one tile of A and B into local memory
+		        for (int la=0; la < LPTA; la++) {
+		            int tid = tidn*RTSM + tidm;
+		            int id = la*RTSN*RTSM + tid;
+		            int row = MOD2(id,TSM);
+		            int col = DIV2(id,TSM);
+		            int tiledIndex = TSK*t + col;
+		            Asub[col][row] = A[tiledIndex*M + offsetM + row];
+		            Bsub[row][col] = B[tiledIndex*N + offsetN + row];
+		        }
+		        // Synchronise to make sure the tile is loaded
+		        barrier();
+
+		        // Loop over the values of a single tile
+		        for (int k=0; k < TSK; k++) {
+
+		            // Cache the values of Bsub in registers
+		            for (int wn=0; wn < WPTN; wn++) {
+		                int col = tidn + wn*RTSN;
+		                Breg[wn] = Bsub[col][k];
+		            }
+
+		            // Perform the computation
+		            for (int wm=0; wm < WPTM; wm++) {
+		                int row = tidm + wm*RTSM;
+		                Areg = Asub[k][row];
+		                for (int wn=0; wn < WPTN; wn++) {
+		                    acc[wm][wn] += Areg * Breg[wn];
+		                }
+		            }
+		        }
+		        // Synchronise before loading the next tile
+		        barrier();
+
+		        // Next tile
+		        t++;
+		    } while (t < numTiles);
+
+		    // Store the final result in C
+		    for (int wm=0; wm < WPTM; wm++) {
+		        int globalRow = offsetM + tidm + wm*RTSM;
+		        for (int wn=0; wn < WPTN; wn++) {
+		            int globalCol = offsetN + tidn + wn*RTSN;
+		            C[globalCol*M + globalRow] = acc[wm][wn];
+		        }
+		    }
+		}
+		'''
 		self.mgr = kp.Manager(gpuIdx)
-		self.matmul_shader_bytes = compileShader(opt_shader_code.format(TS=TS,WPT=WPT,RTS=RTS))
+		#self.matmul_shader_bytes = compileShader(twod_register_code)
+		self.matmul_shader_bytes = compileShader(tiled_shader_code.format(TS=TS,WPT=WPT,RTS=RTS))
 		#self.matmul_shader_bytes = compileShader(naive_matmul_shader_code)
 		M,K,N = MKN
 		self.returnShape = [M,N]
@@ -116,6 +218,7 @@ class SGEMMShader:
 			[self.kpA,self.kpB,self.kpC],  # params
 			self.matmul_shader_bytes,  # spirv
 			(M//TS,N//TS,1),  # workgroup
+			#(M//8,N//8,1), 
 			[float(M),float(K),float(N)],  # spec_consts
 			[]
 		)  # push_consts
@@ -155,9 +258,9 @@ if __name__ == '__main__':
 		import traceback
 		try:
 			M = 1024
-			K = 1024
-			N = 1024
-			shader = SGEMMShader([M,K,N],gpuIdx=0,logLevel=logging.INFO)
+			K = 2048
+			N = 512
+			shader = SGEMMShader([M,K,N],gpuIdx=0,logLevel=logging.DEBUG)
 			shader.showDevice()
 			for i in range(3):
 				print("================================================")
